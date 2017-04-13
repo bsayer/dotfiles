e0920639a31938f136cdd767b1aa18f7a8960197
Ben Sayer <ben@medhub.com>
2017-04-13 10:14:36 -0400

    Add hostname to fish prompt.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,10 @@
 function fish_prompt
   set last_status $status
 
+  if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
+  echo -n -s "$__fish_prompt_hostname" " "
   set_color $fish_color_cwd
   set_color -b 3C3C3C
   printf '%s' (prompt_pwd)
